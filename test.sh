f96935c18e8ab1ca472b18304c5d6f27fb824673
Jiapeng Li <mail@jiapeng.me>
2025-08-26 11:58:55 +0800

    update:

--- test.sh
+++ test.sh
@@ -3,8 +3,12 @@
 CLI=./jpgdtest
 IMAGES=samples/*.jpg
 RESULTS=samples/results.txt
+RESCSV=results.csv
 
 uname -a > $RESULTS
 for img in $IMAGES; do
     $CLI "$img" >> $RESULTS
 done
+
+echo "file,rounds,tjpgd,zjpgd" > $RESCSV
+cat $RESULTS | grep 'file' >> $RESCSV
